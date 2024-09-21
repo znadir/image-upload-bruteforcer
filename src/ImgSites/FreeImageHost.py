6fef754b83455c86f7f3303ab03d191ca007299d
zNadir <115375151+znadir@users.noreply.github.com>
2024-09-21 15:44:15 -0400

    save resources on freeimagehost

--- src/ImgSites/FreeImageHost.py
+++ src/ImgSites/FreeImageHost.py
@@ -1,5 +1,7 @@
 from ImgSite import ImgSite
 import string
+import requests
+import time
 
 
 class FreeImageHost(ImgSite):
@@ -9,3 +11,12 @@ class FreeImageHost(ImgSite):
         id_choices = string.ascii_letters + string.digits, 7
 
         super().__init__(domain_name, test_valid_id, id_choices)
+
+    def check_img(self, img_id:str, timeout = 0):
+        if timeout > 0:
+            time.sleep(timeout)
+
+        # save resources by doing only head request
+        response = requests.head(f'https://{self.domain_name}/{img_id}')
+
+        return response.status_code != 404
